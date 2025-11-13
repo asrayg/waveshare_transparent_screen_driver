cb2357d571251c1fba60728775baa604938dc063
Asray Gopa <asraygopa@gmail.com>
2025-11-13 15:54:33 -0600

    tests changed

--- examples/demo_shapes.py
+++ examples/demo_shapes.py
@@ -1,4 +1,4 @@
-from waveshare_transparent_screen_driver.oled_driver import OLED_1in51
+from waveshare_transparent_oled.oled_driver import OLED_1in51
 from PIL import Image, ImageDraw
 
 disp = OLED_1in51()
